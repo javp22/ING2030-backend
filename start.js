4e1e2c3de5d64a8ab7f85b057a88c52aef391168
dvillas28 <dvillasc@gmail.com>
2025-05-04 15:33:10 -0400

    updated deploy script

--- start.js
+++ start.js
@@ -6,11 +6,11 @@ async function deploy() {
     console.log('Installing dependencies...');
     console.log(output1);
 
-    const { stdout: output2 } = await exec('yarn db:migrate');
+    const { stdout: output2 } = await exec('yarn sequelize-cli db:migrate');
     console.log('Migrating database...');
     console.log(output2);
 
-    const { stdout: output3 } = await exec('yarn db:seed');
+    const { stdout: output3 } = await exec('yarn sequelize-cli db:seed');
     console.log('Seeding database...');
     console.log(output3);
 }
