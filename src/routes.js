3aa1c9aab45ce8bb83e04fa724d1075fc61e20bc
cataanusch <catalina.anusch@uc.cl>
2025-05-05 11:58:22 -0400

    Crear y borrar metas

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 const users = require('./routes/users');
 const transactions = require('./routes/transactions');
 const saving_goals = require('./routes/savinggoals');
-const budget = require('./routes/budget')
+const budget = require('./routes/budget');
+const alert = require('./routes/alerts');
 
 const Router = require('koa-router');
 
@@ -11,5 +12,6 @@ router.use('/users', users.routes());
 router.use('/transactions', transactions.routes());
 router.use('/savinggoals', saving_goals.routes());
 router.use('/budgets', budget.routes());
+router.use('/alerts', alert.routes());
 
 module.exports = router;
